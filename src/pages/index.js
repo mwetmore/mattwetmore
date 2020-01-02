7a11c307fcd5fdef783269cc054b27030f58023c
mwetmore <mwetmore@gmail.com>
2020-01-01 23:10:11 -0500

    GatsbyJS Part 1 Tutorial.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
-
-export default () => <div>Hello world!</div>
+import { Link } from "gatsby"
+export default () => (
+  <div style={{ color: `purple` }}>
+    <Link to="/contact/">Contact</Link>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+)
