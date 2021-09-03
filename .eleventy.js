44f9a24943507b1d209416b1ec19eca072aff953
mwetmore <mwetmore@gmail.com>
2021-09-03 15:23:55 -0400

    Adding eleventy and rookie monster image.

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
-const CleanCSS = require("clean-css");
 module.exports = function(eleventyConfig) {
     // Output directory: _site
   
     // Copy `img/` to `_site/img`
-    eleventyConfig.addPassthroughCopy("social_images");
-    eleventyConfig.addPassthroughCopy({ "node_modules/modern-css-reset/dist/reset.min.css": "includes/reset.min.css" });
-
-    //Clean CSS
-    eleventyConfig.addFilter("cssmin", function(code) {
-      return new CleanCSS({}).minify(code).styles;
-    });
+   // eleventyConfig.addPassthroughCopy("social_images");
+   eleventyConfig.addPassthroughCopy("./src/css");
+    return {
+      dir: {
+        input: './src'
+      },
+      passthroughFileCopy: true
+    };
 };
