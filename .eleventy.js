ff8ac069ef1c49c1ec611ec63412b341d3acf25f
mwetmore <mwetmore@gmail.com>
2021-09-03 15:25:55 -0400

    Adding eleventy and rookie monster image.

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
     // Output directory: _site
   
     // Copy `img/` to `_site/img`
-   // eleventyConfig.addPassthroughCopy("social_images");
+   eleventyConfig.addPassthroughCopy("social_images");
    eleventyConfig.addPassthroughCopy("./src/css");
     return {
       dir: {
